c21bfbdc13858793908b565bffdd2e497bb3edc1
Akash-psg <akashpsgct01@gmail.com>
2024-08-17 12:22:56 +0530

    create project added

---
+++ src/main/java/com/example/demo/Controller/ProjectController.java
@@ -0,0 +1,33 @@
+package com.example.demo.Controller;
+
+
+import com.example.demo.Models.Project;
+import com.example.demo.Service.ProjectService;
+import com.example.demo.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/projects")
+public class ProjectController {
+
+    @Autowired
+    private ProjectService projectService;
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/create")
+    public Project createProject(@RequestBody Project project) {
+        return projectService.createProject(project);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Project>> getAllProjects() {
+        List<Project> projects = projectService.getAllProjects();
+        return ResponseEntity.ok(projects);
+    }
+}
---
+++ src/main/java/com/example/demo/Service/ProjectService.java
@@ -0,0 +1,26 @@
+package com.example.demo.Service;
+
+import com.example.demo.Models.Project;
+import com.example.demo.Repository.ProjectRepository;
+import com.example.demo.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProjectService {
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public Project createProject(Project project) {
+        return projectRepository.save(project);
+    }
+
+    public List<Project> getAllProjects() {
+        return projectRepository.findAll();
+    }
+}
